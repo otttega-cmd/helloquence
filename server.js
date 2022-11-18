8f161cbd63cdc0930b64b4dab7d07f1ac36712f5
Otega <ot.ayomide@gmail.com>
2022-11-18 04:08:35 +0100

    Third Commit

--- server.js
+++ server.js
@@ -5,35 +5,29 @@ const path = require('path');
 const PORT = '3000';
 const HOST_NAME = 'localhost';
 
-
-// Using path to create a path
+// Using path module to create a path
 const blogDbPath = path.join(__dirname, "db", 'blogs.json');
-let blogDB =[];
-
-
-// To be revised further / Why must the server response be in string format?
-let blogs = "";
+let blogDB = [];
 
 
 function requestHandler (req, res) {
     res.setHeader('Content-Type', 'application/json');
 
-//Blogs
+    //Blogs
     // To Get All Blogs
 
     if (req.url === '/blogs' && req.method=== 'GET'){
-        getAllBlogs(req, res)
+        getAllUsers(req, res)
     }else if (req.url === '/blogs' && req.method === 'POST'){
         //create new blog
-        createNewBlog(req, res)
+        createBlog(req, res)
     }else if (req.url === '/blogs' && req.method === 'PUT' ){
         editBlogPost(req, res)
     }else if (req.url === '/blog' && req.method === 'DELETE'){
         deleteBlogPost(req,res)
     }
 }
-
-
+    
 
 
 const getAllBlogs = function (req, res) {
@@ -43,47 +37,31 @@ const getAllBlogs = function (req, res) {
             res.WriteHead(400);
             res.end("An Error Occurred");
         }
+
+        res.end(blogs);
     })
-    res.end(blogs);
+    
 }
-
-const createNewBlog = function (req, res) {
-fs.readFile(blogDbPath, "utf8", (err, blogs)=>{
-    if (err){
-        console.log(err);
-        res.WriteHead(400)
-        res.end(`An Error With Status Code ${err.statusCode} Occurred`)
-    }
-        else {
-    const body = [];
-
-    req.on('data', (chunk)=>{
-        body.push(chunk);
-    })
-
-    req.on('end', ()=>{
-        const parsedBody = Buffer.concat(body).toString();
-        const newBlogPost = JSON.parse(parsedBody);
+console.log("🚀 ~ file: server.js ~ line 53 ~ getAllBlogs ~ getAllBlogs", getAllBlogs)
 
 
-        blogDB.push(newBlogPost);
+// To Create A New Blog Post
+// function createBlog (req, res) {
+//     console.log("🚀 ~ file: server.js ~ line 53 ~ createBlog ~ function")
+//     const body = [];
 
-        //Write Blog Array to Blog JSON File In DB Using FS Module
-        fs.writeFile(blogDbPath, JSON.stringify(blogDB), (err) =>{
-            if(err){
-                console.log(err);
-                res.writeHead(500);
-                res.end(JSON.stringify({
-                    message: `Could not save book to database.`
-                }));
-            }
+//     req.on('data', (chunk) => {
+//         body.push(chunk);
+//     }).end('end', ()=> {
+//         console.log("🚀 ~ file: server.js ~ line 58 ~ req.on ~ .end")
+//         bufferBody = Buffer.concat(body).toString();
+//         parsedBody = JSON.parse(bufferBody);
+//         console.log(parsedBody);
+//         res.WriteHead(200, { 'Content-Type': 'application/json'});
 
-            res.end(JSON.stringify(newBlogPost));
-        })
-    })
-    }   
-})
-}
+//     });
+//     console.log("🚀 ~ file: server.js ~ line 59 ~ req.on ~ req.on")
+// }
 
 const editBlogPost = (req, res) => {
 
@@ -99,9 +77,4 @@ server.listen(PORT, HOST_NAME, ()=> {
     //parse file system for blogDbPath and read sync
     blogDB = JSON.parse(fs.readFileSync(blogDbPath, 'utf8'))
     console.log(`Server is listening on ${HOST_NAME}:${PORT}`);
-})
-// const server = http.createServer(HOST_NAME, PORT, ()=>{
-//     speakerDB=JSON.parse(fs.readFileSync(speakerDBPath, 'utf8'));
-//     console.log(`Server is running on ${HOST_NAME}:${PORT}`)
-// });
-
+});
